2ba58792fe7bdc8d9d3574c8ef2c1551a428e4ab
maaz-afridi20 <maazafridi.ma47@gmail.com>
2024-04-15 12:58:28 +0500

    problem solving...

--- day2/conditional_statements.py
+++ day2/conditional_statements.py
@@ -18,9 +18,10 @@ if a > 10:
 else:
     print('else statement')
 
-#  if-elif-else statement
 
-marks = 70
+print('----------------------------')
+#  if-elif-else statement
+marks = 98
 if marks >= 90:
     print('enjoy the trip')
 elif marks >= 80 and marks < 90:
@@ -29,3 +30,32 @@ elif marks >=70 and marks <80:
     print('new book')
 else:
     print('nothing..')
+
+print('----------------------------')
+print('nested if')
+#  nested if statement.
+
+
+if marks >= 80:
+    print('goog practice')
+    if marks >= 90:
+        print('very good.')
+else:
+    print('noting.')
+
+
+# -------------------------------------------------------------------------
+# short handed if statement.
+#  in the shorthand statement we use the condition and body of if in the same line
+
+if marks >= 90:print('yes true')
+
+# -------------------------------------------------------------------------
+# short handed if else statement.
+#  in the shorthand statement we use the condition and body of if in the same line
+# in this first we will write the body of the if and then the condition and then the else statement
+
+print('new phone') if marks > 90 else print('no phone')
+# so this will print new phone statement because the marks are more than 90
+
+# in the on liner statement of if else we don't use :
---
+++ day2/problem_solving.py
@@ -0,0 +1,50 @@
+# check whether the number is positive or not
+num = int(input('enter any number : '))
+if num > 0:
+    print('positive')
+else:
+    print('negative')
+
+# write program to check whether number is even or odd.
+
+nummy = int(input('Enter any number : '))
+
+if nummy % 2 == 0:
+    print('even')
+else:
+    print('odd')
+
+
+# write a program of area calculator
+
+print("Area calculator...")
+print("choose any number between 1-4")
+print("""
+press 1 to find the area of square
+press 2 to find the ares of rectangle
+press 3 to find the area of circle
+press 4 to find the area of triangle
+""")
+
+choice = int(input('Enter your choice '))
+
+if choice == 1:
+    side = float(input('enter sides '))
+    area = side * side
+    print('area of circle is ', area)
+elif choice == 2:
+    length = float(input('enter length '))
+    width = float(input('enter width '))
+    area = length * width
+    print('area of rectangle is ', area)
+elif choice == 3:
+    radius = float(input('enter radius of circle '))
+    area = ((22/7)*(radius**2))
+    print("area of circle is ", area)
+elif choice == 4:
+    base = float(input('enter triangle base '))
+    height = float(input('enter triangle height '))
+    area = 0.5*base*height
+    print('area of triangle is ', area)
+else:
+    print('Invalid Input ')
