91a08484f9ba5794e2479db20f88657800154dbb
maaz-afridi20 <maazafridi.ma47@gmail.com>
2024-04-15 12:11:25 +0500

    else if and elif statement.

--- day2/conditional_statements.py
+++ day2/conditional_statements.py
@@ -5,15 +5,27 @@ if the, if-else, if-elif-else, nested statement, shorthand if statement
 👉
 """
 
-a = 4
+a = 50
+
+#  if statement.
 if a < 10:
     print('false')
 
 
+#  if else statement.
 if a > 10:
     print('true')
 else:
     print('else statement')
 
+#  if-elif-else statement
 
-
+marks = 70
+if marks >= 90:
+    print('enjoy the trip')
+elif marks >= 80 and marks < 90:
+    print('new phone')
+elif marks >=70 and marks <80:
+    print('new book')
+else:
+    print('nothing..')
