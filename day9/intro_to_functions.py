cecd89970b87aa2b73e71bb86c7eca95bed64f6c
maaz-afridi20 <maazafridi.ma47@gmail.com>
2024-04-22 20:06:03 +0500

    intro to functions and what are arguments and what are parameters

---
+++ day9/intro_to_functions.py
@@ -0,0 +1,35 @@
+"""
+Intro To Functions :
+functions are set of code, which once created, can be reused throughout the program
+anytime anywhere.
+
+functions helps to break the program into smaller parts to make more organize our code.
+
+
+
+Structure of Function :
+
+we have to use def keyword for declaring any function.
+and then the name of the function and then :
+and after this we have to declare the body of the function that what are
+we doing in this function.
+
+
+now for calling this function  we have to use the function name and
+after this we have to use ()
+
+"""
+
+
+def hello():
+    print("Hello World...")
+
+
+hello()
+
+
+def addd(a, b):
+    print(a + b)
+
+
+addd(10, 20)
---
+++ day9/parameters_and_arguments.py
@@ -0,0 +1,15 @@
+"""
+Parameters :
+parameters are those values which are written inside the () after the name of the function.
+
+Arguments :
+arguments are the values passed when we use that function anywhere in our code.
+"""
+
+
+def add_numbers(num1, num2):
+    addition = num1 + num2
+    print(addition)
+
+
+add_numbers(100, 43)
